65704f7226d271dc0769ebd6ad6eed7c7f35e79a
mjamesruggiero <mjamesruggiero@gmail.com>
2016-05-12 09:03:12 -0700

    Basic solver

--- gps1.lisp
+++ gps1.lisp
@@ -11,26 +11,26 @@
   (add-list nil)
   (del-list nil))
 
-(defun GPS (*state* goals *ops*)
-  "General problem solver: acheive all goals using *ops*"
-  (if
-    (every #'acheive goals)
-    'solved))
+(defun appropriate-p (goal op)
+  "An op is appropriate to a goal if its in its add list"
+  (member goal (op-add-list op)))
 
-(defun acheive (goal)
-  "A goal is acheived if it already holds,
+(defun achieve (goal)
+  "A goal is achieved if it already holds,
   or if there is an appropriate op for it that is acceptable"
   (or (member goal *state*)
       (some #'apply-op
             (find-all goal *ops* :test #'appropriate-p))))
 
-(defun appropriate-p (goal op)
-  "An op is appropriate to a goal if its in its add list"
-  (member goal (op-add-list op)))
+(defun GPS (*state* goals *ops*)
+  "General problem solver: achieve all goals using *ops*"
+  (if
+    (every #'achieve goals)
+    'solved))
 
 (defun apply-op (op)
   "Pring a message and update *state* if op is applicable"
-  (when (every #'acheive (op-preconds op))
+  (when (every #'achieve (op-preconds op))
     (print (list 'executing (op-action op)))
     (setf *state* (set-difference *state* (op-del-list op)))
     (setf *state* (union *state* (op-add-list op)))
@@ -45,3 +45,32 @@ according to the keywords. Doesn't alter sequence."
          :test-not (complement test-not) keyword-args)
   (apply #'remove item sequence
          :test (complement test) keyword-args)))
+
+(defparameter *school-ops*
+  (list
+    (make-op 
+      :action 'drive-son-to-school
+      :preconds '(son-at-home car-works)
+      :add-list '(son-at-school)
+      :del-list '(son-at-home))
+    (make-op 
+      :action  'shop-installs-battery
+      :preconds '(car-needs-battery shop-knows-problem shop-has-money)
+      :add-list '(car-works))
+    (make-op 
+      :action 'tell-shop-problem
+      :preconds '(in-communication-with-shop)
+      :add-list '(shop-knows-problem))
+    (make-op 
+      :action 'telephone-shop 
+      :preconds '(know-phone-number)
+      :add-list '(in-communication-with-shop))
+    (make-op 
+      :action 'look-up-number
+      :preconds '(have-phone-book)
+      :add-list '(know-phone-number))
+    (make-op 
+      :action 'give-shop-money
+      :preconds '(have-money)
+      :add-list '(shop-has-money)
+      :del-list '(have-money))))
