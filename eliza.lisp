0d980f5aa45dd4d5942ce95fee174e68e08fe2d0
mjamesruggiero <mjamesruggiero@gmail.com>
2016-05-22 22:39:32 -0700

    More Eliza

--- eliza.lisp
+++ eliza.lisp
@@ -2,9 +2,61 @@
 
 (defconstant no-bindings '((t . t)))
 
+(defun mappend (fn list)
+  "Append the results of calling fn on each element of list.
+  Like mapcon but uses append instead of nconc"
+  (apply #'append (mapcar fn list)))
+
+(defun mklist (x)
+  "Return x if it is a list, otherwise (x)"
+  (if (listp x)
+      x
+      (list x)))
+
 (defun variable-p (x)
   "Is x a variable (a symbol beginning with `?`)"
   (and (symbolp x) (equal (elt (symbol-name x) 0) #\?)))
+
+(defun match-variable (var input bindings)
+  "Does VAR match input? Uses (or updates) and returns bindings"
+  (let ((binding (get-binding var bindings)))
+    (cond ((not binding) (extend-bindings var input bindings))
+          ((equal input (binding-val binding)) bindings)
+          (t fail))))
+
+(defun flatten (the-list)
+  "Append together elements (or lists) in the list"
+  (mappend #'mklist the-list))
+
+(defun starts-with (list x)
+  "Is x a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
+
+(defun segment-pattern-p (pattern)
+  "Is this a segment matching pattern ((?* var) . pat)"
+  (and (consp pattern)
+       (starts-with (first pattern) '?*)))
+
+(defun segment-match (pattern input bindings &optional (start 0))
+  "Match the segment pattern ((?* var) . pat) against input"
+  (let ((var (second (first pattern)))
+        (pat (rest pattern)))
+    (if (null pat)
+        (match-variable var input bindings)
+        ;; We assume that pat starts with a constant
+        ;; In other words, a pattern can't have 2 consecutive vars
+        (let ((pos (position (first pat) input
+                             :start start :test #'equal)))
+          (if (null pos)
+              fail
+              (let ((b2 (pat-match
+                         pat (subseq input pos)
+                         (match-variable var (subseq input 0 pos)
+                                         bindings))))
+                ;; If this match failed, try another longer one
+                (if (eq b2 fail)
+                    (segment-match pattern input bindings (+ pos 1))
+                    b2)))))))
 
 (defun pat-match (pattern input &optional (bindings no-bindings))
   "Match pattern against input in the context of bindings"
@@ -15,9 +67,36 @@
         ((segment-pattern-p pattern)
          (segment-match pattern input bindings))
         ((and (consp pattern) (consp input))
-         (pat-match (rest-pattern) (rest input)
-                    (pat match (first pattern) (first input)
+         (pat-match (rest pattern) (rest input)
+                    (pat-match (first pattern) (first input)
                          bindings)))
         (t fail)))
 
-(defun make-binding ())
+(defun make-binding (var val)
+  (cons var val))
+
+(defun binding-var (binding)
+  "Get the varuable part of a single binding"
+  (car binding))
+
+(defun binding-val (binding)
+  "Get the value part of a single binding"
+  (cdr binding))
+
+(defun get-binding (var bindings)
+  "Find a (variable . val) pair in a binding list"
+  (assoc var bindings))
+
+(defun lookup (var bindings)
+  "Get the value part (for var) from a binding list"
+  (binding-val (get-binding var bindings)))
+
+(defun extend-bindings (var val bindings)
+  "Add a (var . val) pair to a binding list"
+  (cons (cons var val)
+        ;; Once we add a real binding
+        ;; we can get rid of no-bindings
+        (if (eq bindings no-bindings)
+            nil
+            bindings)))
+
