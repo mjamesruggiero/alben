ceb1f96e53ebf20167cfada148802c737bc118a2
mjamesruggiero <mjamesruggiero@gmail.com>
2016-05-29 15:24:59 -0700

    Eliza has debugging tools

--- eliza.lisp
+++ eliza.lisp
@@ -1,6 +1,34 @@
-(defparameter fail nil)
+(defvar *dbg-ids*
+  nil
+  "Identifies used by dbg")
 
-(defparameter no-bindings '((t . t)))
+(defun dbg (id format-string &rest args)
+  "Print debugging info if (DEBUG ID) has been specified"
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+
+(defun set-debug (&rest ids)
+  "Start dbg outpug on the given ids"
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun undebug (&rest ids)
+  "Stop dbg on the ids. With no ids, stop dbg altogether"
+  (setf *dbg-ids* (if (null ids) nil
+                      (set-difference *dbg-ids* ids))))
+
+(defun dbg-indent (id indent format-string &rest args)
+  "Print indented debugging info if (DEBUG ID) has been specified"
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (dotimes (i indent) (princ " " *debug-io*))
+    (apply #'format *debug-io* format-string args)))
+
+(defparameter fail
+  nil)
+
+(defparameter no-bindings
+  '((t . t)))
 
 (defun make-binding (var val)
   (cons var val))
